a9d12218debb057b0554b23fc4e9edf943967451
ItzIsonn_ <kotov.e548@yandex.ru>
2025-02-14 20:32:34 +0300

    updated documentation

--- Meazy/docs/scripts/sections_script.js
+++ Meazy/docs/scripts/sections_script.js
@@ -4,10 +4,10 @@ const pageParam = urlParams.get("page");
 document.querySelectorAll(".page").forEach(info => info.style.display = "none");
 document.querySelectorAll(".pages-buttons a").forEach(info => {
     info.classList.remove("selected");
-    info.href = "index.html?page=" + info.classList[0].split("-")[0];
+    info.href = "index.html?page=" + info.classList[0].replaceAll("-button", "");
 });
 
-if (pageParam == null) window.location = window.location.search + "?page=" + document.querySelector(".page").classList[1].split("-")[0];
+if (pageParam == null) window.location = window.location.search + "?page=" + document.querySelector(".pages-buttons a").classList[0].replaceAll("-button", "");
 else {
     document.querySelector("." + pageParam + "-button").classList.add("selected");
     document.querySelector("." + pageParam + "-page").style.display = '';
