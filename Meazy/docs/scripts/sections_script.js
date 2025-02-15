b6ef75df18b26480b024a60ea5533a755c07d24e
ItzIsonn_ <kotov.e548@yandex.ru>
2025-02-15 20:48:53 +0300

    ability to fold sections

--- Meazy/docs/scripts/sections_script.js
+++ Meazy/docs/scripts/sections_script.js
@@ -1,14 +1,51 @@
-const urlParams = new URLSearchParams(window.location.search);
-const pageParam = urlParams.get("page");
+let activeSections = [];
+if (sessionStorage.getItem('activeSections') != null) {
+    activeSections = sessionStorage.getItem('activeSections').split(" ");
+}
 
 document.querySelectorAll(".page").forEach(info => info.style.display = "none");
 document.querySelectorAll(".pages-buttons a").forEach(info => {
     info.classList.remove("selected");
-    info.href = "index.html?page=" + info.classList[0].replaceAll("-button", "");
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set("page", info.classList[0].replaceAll("-button", ""));
+    info.href = window.location.protocol + "//" + window.location.host + window.location.pathname + "?" + urlParams.toString();
 });
 
-if (pageParam == null) window.location = window.location.search + "?page=" + document.querySelector(".pages-buttons a").classList[0].replaceAll("-button", "");
+document.querySelectorAll(".pages-title").forEach(title => {
+    const selector = title.classList[1].replaceAll("-title", "");
+    const buttons = document.querySelector(".pages-buttons." + selector + "-buttons");
+    buttons.classList.add("hidden");
+
+    title.addEventListener("click", () => {
+        buttons.classList.toggle("hidden");
+        if (!buttons.classList.contains("hidden")) {
+            if (!activeSections.includes(selector)) activeSections.push(selector);
+        }
+        else {
+            activeSections.splice(activeSections.indexOf(selector), 1);
+        }
+
+        if (activeSections.length > 0) {
+            sessionStorage.setItem("activeSections", activeSections.join(" "));
+        }
+        else {
+            sessionStorage.removeItem("activeSections");
+        }
+    })
+})
+
+
+
+const pageParam = new URLSearchParams(window.location.search).get("page");
+if (pageParam == null) {
+    const query = window.location.search.includes("?") ? "&" : "?";
+    window.location = window.location.search + query + "page=" + document.querySelector(".pages-buttons a").classList[0].replaceAll("-button", "");
+}
 else {
     document.querySelector("." + pageParam + "-button").classList.add("selected");
-    document.querySelector("." + pageParam + "-page").style.display = '';
+    document.querySelector(".page." + pageParam + "-page").style.display = '';
+}
+
+for (const buttons of activeSections) {
+    document.querySelector(".pages-buttons." + buttons + "-buttons").classList.remove("hidden");
 }
